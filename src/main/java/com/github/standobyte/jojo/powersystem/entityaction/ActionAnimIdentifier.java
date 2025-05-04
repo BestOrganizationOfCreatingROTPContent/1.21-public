01295ef024e8f18444cc6e31afc770d74e678445
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-05-04 13:46:35 +0300

    Remembered to override ActionAnimIdentifier#hashCode along with equals

--- src/main/java/com/github/standobyte/jojo/powersystem/entityaction/ActionAnimIdentifier.java
+++ src/main/java/com/github/standobyte/jojo/powersystem/entityaction/ActionAnimIdentifier.java
@@ -2,6 +2,7 @@ package com.github.standobyte.jojo.powersystem.entityaction;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.OptionalInt;
 
 import com.github.standobyte.jojo.powersystem.ability.AbilityId;
@@ -48,6 +49,11 @@ public class ActionAnimIdentifier {
 	@Override
 	public boolean equals(Object obj) {
 		return obj instanceof ActionAnimIdentifier other && this.name.equals(other.name) && this.index == other.index;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, index);
 	}
 	
 }
