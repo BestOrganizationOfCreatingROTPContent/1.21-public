bf5181ef001343263f29e7335d3c33bffb69e0d7
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-06-06 22:59:20 +0300

    renamed & removed some sounds

--- src/main/java/com/github/standobyte/jojo/init/ModSoundEvents.java
+++ src/main/java/com/github/standobyte/jojo/init/ModSoundEvents.java
@@ -17,8 +17,6 @@ public class ModSoundEvents {
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PUNCH_LIGHT = SOUNDS.register("stand_punch_light", SoundEvent::createVariableRangeEvent);
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PUNCH_BARRAGE = SOUNDS.register("stand_punch_barrage", SoundEvent::createVariableRangeEvent);
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PUNCH_HEAVY = SOUNDS.register("stand_punch_heavy", SoundEvent::createVariableRangeEvent);
-	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_CRASH = SOUNDS.register("stand_crash", SoundEvent::createVariableRangeEvent);
-	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PARRY = SOUNDS.register("stand_parry", SoundEvent::createVariableRangeEvent);
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_LEAP = SOUNDS.register("stand_leap", SoundEvent::createVariableRangeEvent);
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PUNCH_SWING = SOUNDS.register("stand_punch_swing", SoundEvent::createVariableRangeEvent);
 	public static final DeferredHolder<SoundEvent, SoundEvent> STAND_PUNCH_HEAVY_SWING = SOUNDS.register("stand_punch_heavy_swing", SoundEvent::createVariableRangeEvent);
