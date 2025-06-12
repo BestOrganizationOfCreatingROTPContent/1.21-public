9cd48e0ff2ec466862608a824f057527d8813104
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-06-12 15:36:28 +0300

    changed can_hurt_stands damage type tag namespace

--- src/main/java/com/github/standobyte/jojo/init/ModDamageTypes.java
+++ src/main/java/com/github/standobyte/jojo/init/ModDamageTypes.java
@@ -4,11 +4,12 @@ import com.github.standobyte.jojo.core.JojoMod;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageType;
 
 public class ModDamageTypes {
 	public static final ResourceKey<DamageType> STAND_ATTACK = ResourceKey.create(Registries.DAMAGE_TYPE, JojoMod.resLoc("stand_attack"));
 	
-	public static final TagKey<DamageType> CAN_HURT_STANDS = TagKey.create(Registries.DAMAGE_TYPE, JojoMod.resLoc("can_hurt_stands"));
+	public static final TagKey<DamageType> CAN_HURT_STANDS = TagKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath("jojo", "can_hurt_stands"));
 }
