9b0ae61b78868bf6f4477b9f26df862dc07eb139
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-08-07 21:41:03 +0300

    mannequin item sprites, merged the two creative tabs

--- src/main/java/com/github/standobyte/jojo/init/ModItems.java
+++ src/main/java/com/github/standobyte/jojo/init/ModItems.java
@@ -24,29 +24,35 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+@EventBusSubscriber(modid = JojoMod.MOD_ID)
 public final class ModItems {
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, JojoMod.MOD_ID);
 	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(JojoMod.MOD_ID);
-	
+
 	public static final DeferredItem<Item> DEBUG_ITEM = ITEMS.registerItem("debug_item", DebugItem::new, new Item.Properties());
-	
+
 	public static final DeferredItem<Item> STAND_DISC = ITEMS.registerItem("stand_disc", StandDiscItem::new, new Item.Properties().stacksTo(1));
-	
+
 	public static final DeferredItem<Item> MANNEQUIN = ITEMS.registerItem("mannequin", props -> new MannequinItem(props, false), new Item.Properties().stacksTo(16));
-	
+
 	public static final DeferredItem<Item> MANNEQUIN_SLIM = ITEMS.registerItem("mannequin_slim", props -> new MannequinItem(props, true), new Item.Properties().stacksTo(16));
-	
+
 	public static final DeferredItem<ClothesItem> CLOTHES_BASE_ITEM = ITEMS.registerItem("clothes", props -> new ClothesItem(props));
-	
-	
+
+
 	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN_TAB = CREATIVE_MODE_TABS.register(JojoMod.MOD_ID + "_main", () -> CreativeModeTab.builder()
 			.title(Component.translatable("itemGroup." + JojoMod.MOD_ID + "_main"))
 			.icon(() -> Items.STICK.getDefaultInstance())
 			.displayItems((parameters, output) -> {
+				// most of the mod's items
 				Stream<StandType> stands = StandType.getAllEnabledStands();
 				stands
 				.map(stand -> {
@@ -56,37 +62,35 @@ public final class ModItems {
 				})
 				.forEach(item -> output.accept(item, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS));
 			}).build());
-	
-	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CLOTHES_TAB = CREATIVE_MODE_TABS.register(JojoMod.MOD_ID + "_clothes", () -> CreativeModeTab.builder()
-			.title(Component.translatable("itemGroup." + JojoMod.MOD_ID + "_clothes"))
-			.icon(() -> new ItemStack(CLOTHES_BASE_ITEM.get()))
-			.displayItems((parameters, output) -> {
-				output.accept(MANNEQUIN.get());
-				output.accept(MANNEQUIN_SLIM.get());
-				
-				ClothesItem clothesFactory = CLOTHES_BASE_ITEM.get();
-				
-				parameters.holders()
-				.lookup(JojoRegistries.CLOTHES_SETS_REG_KEY)
-				.ifPresent(
-						clothesSets -> clothesSets.listElements()
-						.flatMap(setHolder -> {
-							List<ClothesDataComponent> components = new ArrayList<>(ClothesSlotType.values().length);
-							ClothesSet set = setHolder.value();
-							for (ClothesSlotType slot : ClothesSlotType.values()) {
-								ClothesPiece piece = set.getPiece(slot);
-								if (piece != null) {
-									components.add(new ClothesDataComponent(setHolder, slot, ClothesPiece.SubClothingPiece.FULL));
-								}
+
+	@SubscribeEvent(priority = EventPriority.LOW)
+	public static void addToModCreativeTabLast(BuildCreativeModeTabContentsEvent event) {
+		if (event.getTabKey() == MAIN_TAB.getKey()) {
+			// items related to clothes
+			event.accept(MANNEQUIN.get());
+			event.accept(MANNEQUIN_SLIM.get());
+
+			// the clothes items themselves
+			ClothesItem clothesFactory = CLOTHES_BASE_ITEM.get();
+
+			event.getParameters().holders()
+			.lookup(JojoRegistries.CLOTHES_SETS_REG_KEY)
+			.ifPresent(
+					clothesSets -> clothesSets.listElements()
+					.flatMap(setHolder -> {
+						List<ClothesDataComponent> components = new ArrayList<>(ClothesSlotType.values().length);
+						ClothesSet set = setHolder.value();
+						for (ClothesSlotType slot : ClothesSlotType.values()) {
+							ClothesPiece piece = set.getPiece(slot);
+							if (piece != null) {
+								components.add(new ClothesDataComponent(setHolder, slot, ClothesPiece.SubClothingPiece.FULL));
 							}
-							return components.stream();
-						})
-						.map(clothesFactory::makeClothesPieceStack)
-						.forEach(item -> output.accept(item, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS))
-						);
-			}).build());
-	
-	
-	
-	
+						}
+						return components.stream();
+					})
+					.map(clothesFactory::makeClothesPieceStack)
+					.forEach(item -> event.accept(item, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS)));
+		}
+	}
+
 }
