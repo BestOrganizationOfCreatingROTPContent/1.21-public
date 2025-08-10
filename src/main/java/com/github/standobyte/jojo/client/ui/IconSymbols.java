8f818d9818841c63409d099539d53ed3e3530382
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-08-10 15:49:19 +0300

    float width symbols are a bad idea after all

--- src/main/java/com/github/standobyte/jojo/client/ui/IconSymbols.java
+++ src/main/java/com/github/standobyte/jojo/client/ui/IconSymbols.java
@@ -8,9 +8,9 @@ import com.github.standobyte.jojo.core.JojoMod;
 public class IconSymbols {
 	// they are kinda small though
 	public static final char LMB_CLICK = IconGlyphsCache.makeCharCodeFor(new IconGlyphInfo(
-			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/left_click.png"), 3, 0, 9, 16, 16, 16), 4.5f, 8));
+			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/left_click.png"), 3, 0, 10, 16, 16, 16), 5, 8));
 	public static final char RMB_CLICK = IconGlyphsCache.makeCharCodeFor(new IconGlyphInfo(
-			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/right_click.png"), 3, 0, 9, 16, 16, 16), 4.5f, 8));
+			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/right_click.png"), 3, 0, 10, 16, 16, 16), 5, 8));
 	public static final char MMB_CLICK = IconGlyphsCache.makeCharCodeFor(new IconGlyphInfo(
-			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/middle_click.png"), 3, 0, 9, 16, 16, 16), 4.5f, 8));
+			new GuiIcon(JojoMod.resLoc("textures/gui/sprites/middle_click.png"), 3, 0, 10, 16, 16, 16), 5, 8));
 }
