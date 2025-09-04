57534872755a4f8d928ac36341ddf56327a54713
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-09-04 14:33:54 +0300

    bruh

--- src/main/java/com/github/standobyte/jojo/client/shader/SeparateBufferEntityShader.java
+++ src/main/java/com/github/standobyte/jojo/client/shader/SeparateBufferEntityShader.java
@@ -23,6 +23,7 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 
+// FIXME fix the crash when you give the stand an enchanted item
 public class SeparateBufferEntityShader {
 	protected ResourceLocation postShaderId;
 	protected String outputShardName;
