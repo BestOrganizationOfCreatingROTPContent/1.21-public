7d6ea5d11eb3605272817c33e47a4525f1e61b46
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-10-27 16:48:05 +0300

    fixed stand arms not rendering in 1st person

--- src/main/java/com/github/standobyte/jojo/client/FirstPersonRender.java
+++ src/main/java/com/github/standobyte/jojo/client/FirstPersonRender.java
@@ -81,6 +81,7 @@ public class FirstPersonRender {
 				StandEntity entity = (StandEntity) povEntity;
 				StandEntityRenderState renderState = standRenderer.createRenderState(entity, partialTick);
 				renderState.visibleParts = HumanoidPart.reduce(renderState.visibleParts, HumanoidPart.ARMS_ONLY);
+				renderState.mayObstructView = false;
 
 				poseStack.pushPose();
 				poseStack.mulPose(Axis.XP.rotationDegrees(renderState.xRot));
